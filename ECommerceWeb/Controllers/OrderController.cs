90895d8d25911985613597d6c61432c7378c4784
Zayana <Zayana@LAPTOP-RDCB0BHG>
2023-11-30 17:01:23 +0530

    Add project files.

---
+++ ECommerceWeb/Controllers/OrderController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using ECommerceWeb.Model;
+
+[ApiController]
+[Route("api/order")]
+public class OrderController : ControllerBase
+{
+    private readonly string _connectionString = "your_database_connection_string_here";
+
+    [HttpPost("getRecentOrder")]
+    public IActionResult GetRecentOrder([FromBody] OrderRequest request)
+    {
+        // Validate the request
+        if (string.IsNullOrEmpty(request.User) || string.IsNullOrEmpty(request.CustomerId))
+        {
+            return BadRequest("Invalid request. User and CustomerId are required.");
+        }
+
+        // Retrieve data from the database using Dapper
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            // Get the customer
+            var customer = connection.QueryFirstOrDefault<Customer>(
+                "SELECT * FROM CUSTOMERS WHERE EMAIL = @Email AND CUSTOMERID = @CustomerId",
+                new { Email = request.User, CustomerId = request.CustomerId });
+
+            if (customer == null)
+            {
+                return BadRequest("Invalid request. User's email address does not match the customer number.");
+            }
+
+            // Get the most recent order for the customer
+            var order = connection.QueryFirstOrDefault<OrderDetails>(
+                "SELECT TOP 1 * FROM ORDERS WHERE CUSTOMERID = @CustomerId ORDER BY ORDERDATE DESC",
+                new { CustomerId = request.CustomerId });
+
+            if (order == null)
+            {
+                return Ok(new OrderResponse { Customer = customer });
+            }
+
+            // Get order items
+            var orderItems = connection.Query<OrderItem>(
+                "SELECT * FROM ORDERITEMS WHERE ORDERID = @OrderId",
+                new { OrderId = order.OrderNumber }).ToList();
+
+            // Replace product name with "Gift" if it contains a gift
+            orderItems.ForEach(item =>
+            {
+                if (item.ContainsGift)
+                {
+                    item.ProductName = "Gift";
+                }
+            });
+
+            // Build the response
+            var response = new OrderResponse
+            {
+                Customer = customer,
+                Order = new OrderDetails
+                {
+                    OrderNumber = order.OrderNumber,
+                    OrderDate = order.OrderDate,
+                    DeliveryAddress = $"{customer.HouseNo} {customer.Street}, {customer.Town}, {customer.PostCode}",
+                    OrderItems = orderItems,
+                    DeliveryExpected = order.DeliveryExpected
+                }
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
---
+++ ECommerceWeb/Model/OrderRequest.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+using System.Security.Cryptography.Xml;
+
+namespace ECommerceWeb.Model
+{
+    public class OrderRequest
+    {
+        public string User { get; set; }
+        public string CustomerId { get; set; }
+    }
+
+    public class OrderResponse
+    {
+        public Customer Customer { get; set; }
+        public OrderDetails Order { get; set; }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string HouseNo { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        public string PostCode { get; set; }
+    }
+
+    public class OrderDetails
+    {
+        public int OrderNumber { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string DeliveryAddress { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public DateTime? DeliveryExpected { get; set; }
+    }
+
+
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public bool ContainsGift { get; set; }
+        public string ProductName { get; set; }
+    }
+
+}
